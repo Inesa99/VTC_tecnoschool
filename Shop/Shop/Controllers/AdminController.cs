c97e9c125723e97a070a7e478dcaf755b9ded4f4
Inesa99 <inesamkrtchyan9999@gmail.com>
2023-04-05 11:52:52 +0400

    Working wit db

---
+++ Shop/Shop/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ShopContext _context;
+
+        public AdminController(ShopContext shopContext)
+        {
+            _context = shopContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
